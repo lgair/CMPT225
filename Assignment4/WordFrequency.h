fd24046a6508fe9e131a7499ca52ce602a081a9d
laika <laika.catt@gmail.com>
2017-10-30 16:08:50 -0700

    .h file done

--- Assignment4/WordFrequency.h
+++ Assignment4/WordFrequency.h
@@ -1,13 +1,29 @@
 #include <iostream>
-
-using namespace std;
-
-class WordFrequency
+#include <string>                                                                                         class WordFrequency
 {
-
 public:
+	WordFrequency();// Default Constructor
+	WordFrequency(string word);// Copy Constructor
+	WordFrequency();// Destructor
+	WordFrequency & operator=(WordFrequency obj); //Overloaded Assignment operator
+	
+	//Accessors and Mutators
+	//PRE: WordFrequency object exists or is in scope
+	//PARAM: N/A
+	//POST: increments the frequency of the word
+	void increment();
+	
+	//PRE: WordFrequency object exists or is in scope
+	//PARAM: N/A
+	//POST: Returns the current frequency value
+	int getFrequency() const;
 
-
+	//PRE: WordFrequency object exists or is in scope
+	//PARAM: N/A
+	//POST: Returns word value
+	string getWord() const;
 private:
-
+string word;
+int frequency;
 };
+
