f19dc469bbd2fa1dbda55b671b0257ae63fa9d7f
laika <laika.catt@gmail.com>
2017-10-30 15:26:27 -0700

    Test file complete

--- lab6/test.cpp
+++ lab6/test.cpp
@@ -2,11 +2,15 @@
 #include <sstream>
 #include <stdexcept>
 #include "Fraction.h"
-
-
 using namespace std;
 
-
 int main(){
-
+	Fraction fr1(3,4);
+	Fraction fr2(5,8);
+	cout << fr1 <<" == " << fr2 << endl << (fr1 == fr2? "true" : "False") << endl;
+	cout << fr1 <<" != " << fr2 << endl << (fr1 != fr2? "true" : "False") << endl;
+	cout << fr1 <<" < " << fr2 << endl << (fr1 < fr2? "true" : "False") << endl;
+	cout << fr1 <<" <= " << fr2 << endl << (fr1 <= fr2? "true" : "False") << endl;
+	cout << fr1 <<" > " << fr2 << endl << (fr1 > fr2? "true" : "False") << endl;
+	cout << fr1 <<" >= " << fr2 << endl << (fr1 >= fr2? "true" : "False") << endl;
 }
