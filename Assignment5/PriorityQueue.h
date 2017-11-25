f67321e0a1fd746f807db4072b17dd20b6f005e7
laika <laika.catt@gmail.com>
2017-11-25 14:53:05 -0800

    sorting function to be completed for A5

--- Assignment5/PriorityQueue.h
+++ Assignment5/PriorityQueue.h
@@ -12,28 +12,10 @@ class PriorityQueue{
 		int queueSize;
 		int arrSize;
 		
-		//PRE:
-		//PARAM:
-		//POST:
-		/*int heapify(){
-			int parent = (queueSize - 1)/2;
-			for (int i = ((queueSize - 1)/2); i <= 0; i--){
-				if (arr[i] > arr[2i+1] && arr[2i+2]) continue;
-				else if( arr[i] < arr[2i+1] || arr[2i+2]){
-					if(arr[2i+2] > arr[i]){
-						arr[i] = arr[i] + arr[2i+2];
-						arr[2i+2]=arr[i]-arr[2i+2];
-						arr[i] = arr[i] - arr[2i+2];
-					}
-					if(arr[2i+1] > arr[i]){
-						arr[i] = arr[i] + arr[2i+1];
-						arr[2i+1]=arr[i]-arr[2i+1];
-						arr[i] = arr[i] - arr[2i+1];
-					}
-					
-				}
-			}
-		}; // heapifies array of type T variables*/
+		//PRE: N/A
+		//PARAM:arr = arr of template variables n = size of array
+		//POST: heapifies array of template variables
+		int heapify(int* arr, int n){}; // heapifies array of type T variables*/
 	public:
 		//Constructors
 		PriorityQueue();
@@ -47,29 +29,30 @@ class PriorityQueue{
 		//------------------Data methods----------------------
 		
 		//PRE:
-		//PARAM:
-		//POST:
+		//PARAM: data = template variable to insert
+		//POST: inserts into ADT and maintains MAXHEAP properties
 		void insert(int data);
 		
-		//PRE:
-		//PARAM:
-		//POST:
+		//PRE: obj exists
+		//PARAM: N/A
+		//POST: Extracts value with highest priority and ensures max heap 
+		//properties are maintaned
 		int remove();
 		
 		//PRE: object initailized
 		//PARAM: N/A
 		//POST: returns item in the heap with the highest priority
-		int peek() const;//done
+		int peek()const;//done
 
 		//PRE: object initialized
 		//PARAM:N/A
 		//POST:returns QueueSize
-		int size() const;//done
+		int size()const;//done
 
 		//PRE: Object initailized
 		//PARAM: N/A
 		//POST: Returns ArrSize
-		int maxSize() const;//done
+		int maxSize()const;//done
 
 		//PRE: object initialized
 		//PARAM: N/A
@@ -79,15 +62,22 @@ class PriorityQueue{
 		//PRE: Object initailized
 		//POST: sorts the heap
 		//PARAM; arr = arr to sort, size = ArrSize
-		int* sort(int arr, int size);
+		void sort(int* arr, int n);
 
 		//PRE: object initialized
 		//PARAM: N/A
 		//POST: Returns pointer to PriorityQueue's underlying arr
-		int* contents();//done
-		//utility
-		void bubbleup(int size);
+		int* contents()const;//done
+
+		//---------------------utility---------------------------
+		void bubbleup(int i);
 		void swapAtIndex(int i, int j);
+		void bubbleDown(int i);
+		
+		//PRE: Object exists
+		//PARAM: Initial call MUST index from the root (i = 0)
+		//POST: returns true if the heap property exists else false
+		bool isHeap(int i)const;
 };
 
 //Constructors
@@ -95,17 +85,18 @@ class PriorityQueue{
 PriorityQueue::PriorityQueue(): arrSize (4), queueSize(0), arr (new int[arrSize]){}
 //template <class T>
 PriorityQueue::PriorityQueue(int* arr,int n): arrSize (2*n),  queueSize (n){
-	for (int i = 0; i < queueSize; i++){
+	for (int i = 0; i < queueSize-1; i++){
 		insert(arr[i]);
 	}
 }
+
 //Copy Constructor
 PriorityQueue::PriorityQueue(PriorityQueue & obj){
 	cerr<<"copyflag"<<endl;
 	queueSize = obj.size();
 	arrSize = obj.maxSize();
 	arr = new int[arrSize];
-	for(int i = 0; i < queueSize; i++){arr[i] = obj.arr[i];}
+	for(int i = 0; i < queueSize-1; i++){arr[i] = obj.arr[i];}
 }
 
 //Overlaoded assignment operator
@@ -134,9 +125,41 @@ void PriorityQueue::insert(int data){
 		delete[] temp;
 	}
 	arr[queueSize] = data;
-	bubbleup(queueSize);
+	if(!isHeap(0)){bubbleup(queueSize);}
 	queueSize++;
 }
+
+int PriorityQueue::remove(){
+	if(empty()){throw std::runtime_error("Empty array");}
+	int root = arr[0];//root should be largest value in the queue
+	arr[0] = arr[queueSize-1];
+	if(!isHeap(0)){bubbleDown(0);}
+	queueSize--;
+	return root;
+}
+
+int PriorityQueue::peek()const{
+	if(empty()){return arr[0];}
+	else{throw std::runtime_error("Empty array");}//try catch block?
+}
+
+//Getters
+int PriorityQueue::size() const {return queueSize;}
+int PriorityQueue::maxSize() const {return arrSize;}
+
+//isEmpty test
+bool PriorityQueue::empty() const {return (queueSize == 0 ? true:false);}
+
+// in-place sort
+void PriorityQueue::sort(int* arr, int n){
+	heapify(arr, n);
+}
+
+//direct access to private variable
+//template <class T>
+int* PriorityQueue::contents() const {return arr;}
+
+//Utility
 void PriorityQueue::bubbleup(int i){
 	int parent = (i - 1)/2;
 	if (arr[i] > arr[parent]){
@@ -144,19 +167,43 @@ void PriorityQueue::bubbleup(int i){
 		bubbleup(parent);
 	}
 }
+void PriorityQueue::bubbleDown(int i){
+	int parent = i;
+	int leftChild = 2*i+1;
+	int rightChild = 2*i+2;
+	if(leftChild < queueSize && arr[parent] > arr[leftChild]){
+		parent = leftChild;
+	}
+	if(rightChild < queueSize && arr[parent] > arr[rightChild]){
+		parent = rightChild;
+	}
+	//check children and swap to correct places
+	if(parent != i){
+		swapAtIndex(leftChild,rightChild);
+		bubbleDown(parent);
+	}
+}
+
+// standard swap function
 void PriorityQueue::swapAtIndex(int i, int j){
 	int temp = arr[i];
 	arr[i] = arr[j];
 	arr[j] = temp;
 }
-int PriorityQueue::remove(){
+
+//checks if the heap property is preserved
+bool PriorityQueue::isHeap(int i)const{
+	if(2*i+1 > queueSize-1){return true;}//i has no children
+	else if(2*i+2 > queueSize-1){// checks node with only left children
+		return arr[i] < arr[2*i+1];
+	}
+	//i has two children
+	else{
+		if((arr[i] < arr[2*i+1])&&(arr[i] < arr[2*i+2])){
+			return isHeap(2*i+1) && isHeap(2*i+2);
+		}
+		else {
+			return false;
+		}
+	}
 }
-int PriorityQueue::peek()const{
-	if(empty()){return arr[0];}
-	else{throw std::runtime_error("Empty array");}//try catch block?
-}
-int PriorityQueue::size() const {return queueSize;}
-int PriorityQueue::maxSize() const {return arrSize;}
-bool PriorityQueue::empty() const {return (queueSize == 0 ? true:false);}
-//template <class T>
-int* PriorityQueue::contents()  { return arr;}
